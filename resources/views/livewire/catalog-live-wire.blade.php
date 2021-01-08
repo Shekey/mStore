dc9e9c68d75b9cde06682dbec84f550d459649c1
Shekey <ajdin_sah@hotmail.com>
2021-01-08 15:03:01 +0100

    fix object cover

--- resources/views/livewire/catalog-live-wire.blade.php
+++ resources/views/livewire/catalog-live-wire.blade.php
@@ -284,7 +284,7 @@
         .swiper-wrapper img {
             width: 100%;
             height: 100%;
-            object-fit: cover;
+            object-fit: contain;
         }
 
         .articles > div:hover {
--- resources/views/partials/list-markets.blade.php
+++ resources/views/partials/list-markets.blade.php
@@ -1,7 +1,7 @@
 <div class="text-black body-font flex flex-col w-full md:w-1/2 lg:w-1/3 mb-3 md:mx-2 lg:mx-4 relative fade fadeOut">
     <div class="mx-auto flex flex-wrap w-full px-0 py-4 md:pr-8 flex-col justify-center ">
         <div class="lg:flex-grow pl-0 items-center flex flex-col items-center text-center">
-            @if($i->points > 1)<h1 class="title-font text-3xl mb-2 font-medium text-gray-900" style="flex-basis: 100%;"><a href="/prodavnica/{{ $i->id }}" class="capitalize flex items-center">{{ $i->name }}<span class="ml-4 inline-block bg-gray-900 text-white px-2 rounded-full uppercase font-semibold text-sm tracking-wide">{{ $i->points }} @if($i->points > 1) BODA @else BOD @endif GRATIS</span></a></h1>@endif
+            @if($i->points >= 1)<h1 class="title-font text-3xl mb-2 font-medium text-gray-900" style="flex-basis: 100%;"><a href="/prodavnica/{{ $i->id }}" class="capitalize flex items-center">{{ $i->name }}<span class="ml-4 inline-block bg-gray-900 text-white px-2 rounded-full uppercase font-semibold text-sm tracking-wide">{{ $i->points }} @if($i->points > 1) BODA @else BOD @endif GRATIS</span></a></h1>@endif
             <p class="mb-3 font-medium mt-4">
                 {!! $i->freeDelivery == 0 ? '<span class="inline-block bg-orange-300 text-black px-2 rounded-full uppercase font-semibold text-xl tracking-wide">Dostava se plaća (' . $i->orderPaid . 'KM )</span>' : '<span class="inline-block bg-gray-800 text-white text-xl px-2 rounded-full uppercase font-semibold tracking-wide">Besplatna dostava</span>'  !!}
             </p>
@@ -16,9 +16,9 @@
         </div>
         <a href="/prodavnica/{{ $i->id }}" class="w-full mb-5 text-center md:mb-0 order-first flex items-center market-item-img aspect-100">
             @if (!App::environment('production'))
-                <img class="object-cover object-center rounded inline-block mx-auto lozad h-full w-full" alt="hero" data-src="/storage/{{ $i->image }}" style="z-index: 10;">
+                <img class="object-contain object-center rounded inline-block mx-auto lozad h-full w-full" alt="hero" data-src="/storage/{{ $i->image }}" style="z-index: 10;">
             @else
-                <img class="object-cover object-center rounded inline-block mx-auto lozad h-full w-full" alt="hero" data-src="/public/storage/{{ $i->image }}" style="z-index: 10;">
+                <img class="object-contain object-center rounded inline-block mx-auto lozad h-full w-full" alt="hero" data-src="/public/storage/{{ $i->image }}" style="z-index: 10;">
 
             @endif
         </a>
